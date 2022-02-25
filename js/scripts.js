df7efc3e26179ee0cc489ca73f09b484f03eb75f
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-02-25 10:08:04 +0100

    creao elemento html

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,33 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
  * 
  */
 
+const wrapper = document.getElementById('for-wrapper');
+
 for (let i = 1; i <= 100; i++) {
+    // creo il div di riferimento
+    let newDiv = document.createElement('div');
 
     if (i % 3 === 0 && i % 5 === 0) {
+        // cambio il contenuto del div appena creato inserendovi il valore dell'indice
+
+
+        newDiv.innerHTML = "FizzBuzz";
         console.log("FizzBuzz");
     } else if (i % 3 === 0) {
+        newDiv.innerHTML = "Fizz";
         console.log("Fizz");
     } else if (i % 5 === 0) {
+        newDiv.innerHTML = "Buzz";
         console.log("Buzz");
     } else {
         console.log(i);
+        newDiv.innerHTML = i;
     }
+
+    // aggiungo al div appena creato le classi necessarie
+    newDiv.classList.add("box", "m-3", "fs-2");
+
+    // infine aggiungo il div creato al parent al quale voglio aggiungerlo
+    wrapper.append(newDiv);
 
 }
